8ee1629c04ec1eb3aa6bf12e31b55d4d9e8096f7
Andrej_Klimov <mrandyk2@gmail.com>
2022-08-06 12:35:53 +0200

    added cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import ProductCard from "./features/products/ProductCard";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+          <ProductCard
+              name="T-Shirt"
+              description="nice shirt, bro"
+              price={20} />
+          <ProductCard
+              name='Bucket hat'
+              description='for your empty head'
+              price={10} />
+          <ProductCard
+              name='Fleece joggers'
+              description='best for summer'
+              price={40}/>
       </header>
     </div>
   );
